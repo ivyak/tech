8ee4783419da03d726026ba1c6cb44b3c4bf53e1
ivyak <bulletfall47@gmail.com>
2026-02-10 01:11:03 +0900

    CustomUserとTaskモデル作成完了

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from app.models import CustomUser, Task
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Task)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    pass
+
+class Task(models.Model):
+    class Level(models.TextChoices):
+        NORMAL = "NO", "普通"
+        IMPORTANT = "IM", "重要"
+    
+    title = models.CharField(max_length=20, null=False, blank=False)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_done = models.BooleanField(default=False)
+    level = models.CharField(max_length=2, choices=Level.choices, default=Level.NORMAL)
+    photo = models.ImageField(upload_to="task/", null=True, blank=True)
+
+    def __str__(self):
+        return self.title
